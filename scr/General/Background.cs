a18751d924307ca703b7343346acf38d67346398
jam0824 <session_fixation@hotmail.co.jp>
2024-07-14 12:59:39 +0900

    背景と霧をつけた

--- scr/General/Background.cs
+++ scr/General/Background.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    public float BgSpeed = 0.01f;
+    public float scrollSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        scrollSpeed = GameObject.Find("StageManager").GetComponent<StageManager>().scrollSpeed;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (scrollSpeed == null) return;
         Vector3 pos = this.transform.position;
-        pos.z += BgSpeed;
+        pos.z -= scrollSpeed;
         this.transform.position = pos;
     }
 }
